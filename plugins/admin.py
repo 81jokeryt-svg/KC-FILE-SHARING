67223ef99f5ee0b3b9dae4b1cbedfb283198bea0
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-05-24 08:16:47 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -3,6 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import ADMINS
 from plugins.dbusers import *
+from utils import *
 
 @Client.on_message(filters.command("admin") & filters.user(ADMINS))
 async def admin_panel(client, message):
