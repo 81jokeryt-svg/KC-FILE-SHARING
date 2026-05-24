962a386b659a9db4f7afdb2f9ba71ee9c989afdd
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-05-24 15:59:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -136,6 +136,12 @@ async def start(client, message):
     # 2. HANDLE BATCH LINKS
     elif data.split("-", 1)[0] == "BATCH":
         try:
+            is_user_premium = await db.check_premium_status(user_id) if hasattr(db, 'check_premium_status') else False
+        
+           if not is_user_premium: # <--- 'set' की जगह 'not' करें
+               if settings.get("premium_mode", False):
+                   await message.reply_text("👑 **यह फाइल प्रीमियम है!**\n\nइसे एक्सेस करने के लिए कृपया प्रीमियम लें।")
+                   return 
             # 🌟 MODIFIED BYPASS: Agar user premium hai toh token check bypass ho jayega
             if not is_premium and is_verify_mode == True and not await check_verification(client, user_id):
                 btn = [[
