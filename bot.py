3b834f431720930c55a44ec893ab6f10ab71bee1
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-05-25 09:46:21 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,17 +1,28 @@
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 import sys
 import glob
-import pyromod 
 import importlib
 from pathlib import Path
 from pyrogram import idle
 import logging
 import logging.config
 
+# Don't Remove Credit Tg - @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+# Don't Remove Credit Tg - @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
@@ -23,29 +34,39 @@ from datetime import date, datetime
 import pytz
 from aiohttp import web
 from TechVJ.server import web_server
+
+# Don't Remove Credit Tg - @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 import asyncio
+from pyrogram import idle
 from plugins.clone import restart_bots
 from TechVJ.bot import StreamBot
 from TechVJ.utils.keepalive import ping_server
 from TechVJ.bot.clients import initialize_clients
 
+# Don't Remove Credit Tg - @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
+StreamBot.start()
+loop = asyncio.get_event_loop()
+
+# Don't Remove Credit Tg - @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
 
 async def start():
     print('\n')
     print('Initalizing Tech VJ Bot')
-    
-    # BOT KO YAHAN ASYNC LOOP KE ANDAR START KARNA HAI
-    await StreamBot.start() 
-    
     bot_info = await StreamBot.get_me()
     StreamBot.username = bot_info.username
     await initialize_clients()
-    
-    # YAHAN AAPKE SAARE COMMANDS (PLUGINS) LOAD HONGE
     for name in files:
         with open(name) as a:
             patt = Path(a.name)
@@ -57,33 +78,34 @@ async def start():
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
             print("Tech VJ Imported => " + plugin_name)
-            
     if ON_HEROKU:
         asyncio.create_task(ping_server())
-        
     me = await StreamBot.get_me()
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    
     app = web.AppRunner(await web_server())
     await StreamBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
     await app.setup()
     bind_address = "0.0.0.0"
     await web.TCPSite(app, bind_address, PORT).start()
-    
     if CLONE_MODE == True:
         await restart_bots()
-        
     print("Bot Started Powered By @VJ_Bots")
     await idle()
 
+# Don't Remove Credit Tg - @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
 if __name__ == '__main__':
-    # EVENT LOOP YAHAN CREATE KAREIN TAaKI WARNING NA AAYE
-    loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
+
+
+# Don't Remove Credit Tg - @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
